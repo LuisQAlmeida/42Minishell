3f5391ca70469395b80c14095760f519dd8448b2
Luis Quental Almeida <luis.quental.almeida@gmail.com>
2026-06-01 23:30:21 +0100

    MSH-21 feat: add interactive main loop skeleton

---
+++ src/shell/main_loop.c
@@ -0,0 +1,34 @@
+#include <readline/readline.h>
+#include "minishell.h"
+
+static int	is_blank_line(const char *line)
+{
+	size_t	i;
+
+	i = 0;
+	while (line[i] && ms_isspace(line[i]))
+		i++;
+	return (line[i] == '\0');
+}
+
+int	main_loop(char **envp)
+{
+	char	*line;
+	int		status;
+
+	status = 0;
+	while (1)
+	{
+		line = readline("minishell$ ");
+		if (!line)
+			break ;
+		if (is_blank_line(line))
+		{
+			free(line);
+			continue ;
+		}
+		status = run_once(line, envp);
+		free(line);
+	}
+	return (status);
+}
