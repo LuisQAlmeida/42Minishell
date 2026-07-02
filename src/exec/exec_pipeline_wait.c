0b9ad1b905fe8043b2c9bbd69e7e1555a780bc59
Luis Quental Almeida <luis.quental.almeida@gmail.com>
2026-07-02 19:11:31 +0100

    MSH-85 clean pipeline signal output

--- src/exec/exec_pipeline_wait.c
+++ src/exec/exec_pipeline_wait.c
@@ -1,11 +1,27 @@
 #include "minishell.h"
 
+static void	print_signal_msg(int status)
+{
+	int	sig;
+
+	if (!WIFSIGNALED(status))
+		return ;
+	sig = WTERMSIG(status);
+	if (sig == SIGINT)
+		write(STDOUT_FILENO, "\n", 1);
+	else if (sig == SIGQUIT)
+		ft_putendl_fd("Quit (core dumped)", STDERR_FILENO);
+}
+
 static int	status_from_wait(int status)
 {
 	if (WIFEXITED(status))
 		return (WEXITSTATUS(status));
 	if (WIFSIGNALED(status))
+	{
+		print_signal_msg(status);
 		return (128 + WTERMSIG(status));
+	}
 	return (1);
 }
 
